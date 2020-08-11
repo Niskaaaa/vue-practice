c6b75eae75fdbc9014488d6a8ccd8aae8c19aedd
Niskaaaa <49870606+Niskaaaa@users.noreply.github.com>
2020-08-11 14:25:32 +0800

    webpack

--- webpackTest/src/main.js
+++ webpackTest/src/main.js
@@ -1,9 +1,22 @@
 import $ from 'jquery'
 
+import './css/index.css'
+
+import './css/index.less'
+import 'bootstrap/dist/css/bootstrap.css'//默认去node_modules去找
+
 
 $(function(){
-    $('li:odd').css('backgroundColor','red')
+    $('li:odd').css('backgroundColor','pink')
     $('li:even').css('backgroundColor',function(){
         return '#'+'D97634'
     })
 })
+
+class Person{
+
+    //静态属性，可以直接通过类名访问的属性；实例属性，只能通过类的实例来访问
+    static info = {name:'zs',age:20}
+}
+
+var pl=new Person()
--- webpackTest/webpack.config.js
+++ webpackTest/webpack.config.js
@@ -1,3 +1,32 @@
+//配置文件路径
+const path = require('path'); 
+const webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
-    
+    entry: './src/main.js',//打包文件入口
+    output:{
+        path: path.resolve(__dirname,'dist'),//出口
+        filename:'bundle.js'
+    },
+
+    plugins:[
+        new webpack.HotModuleReplacementPlugin(),
+        new htmlWebpackPlugin({
+            template:path.resolve(__dirname,'./src/index.html'),
+            filename:'index123.html'
+        })
+    ],
+    module:{
+        rules:[
+            {test:/\.css$/,use:['style-loader','css-loader']},
+            {test:/\.less$/,use:['style-loader','css-loader','less-loader']},//sass同理
+            {test:/\.{jpg|png|gif|bmp|jpeg}$/,use:'url-loader'},
+            {test:/\.{ttf|eot|svg|woff|woff2}$/,use:'url-loader'},
+            {test:/\.js$/,use:'babel-loader',exclude:/node_modules/}
+        ]
+    }
+
+
+
 }
